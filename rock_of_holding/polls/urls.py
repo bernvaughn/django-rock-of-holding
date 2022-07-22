b800293315f210625260bf0941980eb37d142484
Vaughn B <vaughnjbernard@gmail.com>
2022-07-22 16:03:05 -0400

    join room functionality

--- rock_of_holding/polls/urls.py
+++ rock_of_holding/polls/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path('', PollView.as_view()),
     path('createpoll', CreatePollView.as_view()),
-    path('getroom',GetRoomView.as_view())
+    path('getroom',GetRoomView.as_view()),
+    path('joinroom',JoinPollView.as_view())
 ]
--- rock_of_holding/polls/views.py
+++ rock_of_holding/polls/views.py
@@ -19,13 +19,30 @@ class GetRoomView(APIView):
         code = request.GET.get(self.lookup_url_kwarg)
         if code != None:
             poll = Poll.objects.filter(code=code).first()
-            if poll.exists():
+            if poll is not None:
                 data = PollSerializer(poll[0]).data
                 data['is_host'] = self.request.session.session_key == poll[0].host
                 return Response(data,status=status.HTTP_200_OK)
             return Response({'Room not found': 'Invalid Room Code'}, status=status.HTTP_404_NOT_FOUND)
         return Response({'Bad Request': 'Code paramater not found in request.'},status=status.HTTP_400_BAD_REQUEST)
 
+
+class JoinPollView(APIView):
+    lookup_url_kwarg='code'
+
+    def post(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        code = request.data.get(self.lookup_url_kwarg)
+        if code != None:
+            poll = Poll.objects.filter(code=code).first()
+            if poll is not None:
+                self.request.session['room_code'] = code
+                return Response({'message': 'Room joined!'}, status=status.HTTP_200_OK)
+            return Response({'Bad Request': 'Invalid room code'}, status=status.HTTP_400_BAD_REQUEST)
+            
+        return Response({'Bad Request': 'Invalid post data, did not find a matching room'}, status=status.HTTP_400_BAD_REQUEST)
 
 class CreatePollView(APIView):
     serializer_class = CreatePollSerializer
@@ -50,6 +67,7 @@ class CreatePollView(APIView):
                 poll.title = title
                 poll.save()
 
+            self.request.session['room_code'] = poll.code
             return Response(PollSerializer(poll).data, status=status.HTTP_201_CREATED)
 
         return Response({'Bad Request': 'Invalid data'}, status=status.HTTP_400_BAD_REQUEST)
