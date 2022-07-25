07e66abd3188fa91a2efb1e0125a4d7ffa23b698
Vaughn B <vaughnjbernard@gmail.com>
2022-07-25 09:24:34 -0400

    added homepage functionality

--- rock_of_holding/polls/urls.py
+++ rock_of_holding/polls/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', PollView.as_view()),
     path('createpoll', CreatePollView.as_view()),
     path('getroom',GetRoomView.as_view()),
-    path('joinroom',JoinPollView.as_view())
+    path('joinroom',JoinPollView.as_view()),
+    path('userinroom',UserInRoom.as_view()),
 ]
--- rock_of_holding/polls/views.py
+++ rock_of_holding/polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from rest_framework import generics, status
 from .models import *
@@ -71,3 +71,15 @@ class CreatePollView(APIView):
             return Response(PollSerializer(poll).data, status=status.HTTP_201_CREATED)
 
         return Response({'Bad Request': 'Invalid data'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserInRoom(APIView):
+    def get(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        data = {
+            'code': self.request.session.get('room_code')
+        }
+
+        return JsonResponse(data, status=status.HTTP_200_OK)
